840b42378bb6d280f1ae8ee9f0a8a674d46fd72c
isaac545454 <isaac.newton1221@gmail.com>
2023-09-22 18:56:50 -0300

    test: ensure RequiredFiledValidation returns falsy  error if field is not  empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,16 @@
 import { RequiredFieldError } from '../errors/required-field-error';
 import { RequiredFieldValidation } from './required-field-validation';
+import { faker } from '@faker-js/faker';
 
 describe('RequiredFieldValidation', () => {
 	test('should return error if field is empty', () => {
 		const sut = new RequiredFieldValidation('email');
-		const error = sut.validate('email');
+		const error = sut.validate('');
 		expect(error).toEqual(new RequiredFieldError());
+	});
+	test('should return false if field is not empty', () => {
+		const sut = new RequiredFieldValidation('email');
+		const error = sut.validate(faker.animal.cat());
+		expect(error).toBeFalsy();
 	});
 });
