1d25ae94638b2854784670e533806b1eb19985ae
isaac545454 <isaac.newton1221@gmail.com>
2023-09-22 20:11:59 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,7 +4,11 @@ import { FieldValidation } from '@/validation/protocols/field-validation';
 export class EmailValidation implements FieldValidation {
 	constructor(readonly field: string) {}
 
-	validate(value: string): Error {
+	validate(value: string): Error | null {
+		const emailRegex =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+		if (emailRegex.test(value)) return null;
 		return new InvalidFielError();
 	}
 }
