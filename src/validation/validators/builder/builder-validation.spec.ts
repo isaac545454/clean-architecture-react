15bc6ecaa556042182101584ad6da2e81c9d253c
isaac545454 <isaac.newton1221@gmail.com>
2023-09-23 11:42:08 -0300

    test: ensure ValidationBuilder returns  list of  validations

--- src/validation/validators/builder/builder-validation.spec.ts
+++ src/validation/validators/builder/builder-validation.spec.ts
@@ -2,18 +2,33 @@ import { EmailValidation } from '../email/email-validation';
 import { MinLenthValidation } from '../min-lenght/min-lenght-valitaion';
 import { RequiredFieldValidation } from '../required-field/required-field-validation';
 import { ValidationBuilder as sut } from './builder-validation';
+import { faker } from '@faker-js/faker';
 
 describe('ValidationBuilder', () => {
 	test('should return RequiredFielValidation', () => {
-		const validations = sut.field('any_field').required().build();
-		expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+		const field = faker.database.column();
+		const validations = sut.field(field).required().build();
+		expect(validations).toEqual([new RequiredFieldValidation(field)]);
 	});
 	test('should return EmailValidation', () => {
-		const validations = sut.field('any_field').email().build();
-		expect(validations).toEqual([new EmailValidation('any_field')]);
+		const field = faker.database.column();
+		const validations = sut.field(field).email().build();
+		expect(validations).toEqual([new EmailValidation(field)]);
 	});
 	test('should return MinLenthValidation', () => {
-		const validations = sut.field('any_field').min(5).build();
-		expect(validations).toEqual([new MinLenthValidation('any_field', 5)]);
+		const field = faker.database.column();
+		const number = faker.number.int();
+		const validations = sut.field(field).min(number).build();
+		expect(validations).toEqual([new MinLenthValidation(field, number)]);
+	});
+	test('should return list of validations', () => {
+		const field = faker.database.column();
+		const number = faker.number.int();
+		const validations = sut.field(field).required().email().min(number).build();
+		expect(validations).toEqual([
+			new RequiredFieldValidation(field),
+			new EmailValidation(field),
+			new MinLenthValidation(field, number),
+		]);
 	});
 });
