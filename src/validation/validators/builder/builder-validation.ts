70f9c3840edf2d857b0fbecc2a91c09ed8e24312
isaac545454 <isaac.newton1221@gmail.com>
2023-09-23 11:12:50 -0300

    feat: ensure validationBuilder has a email   helper

--- src/validation/validators/builder/builder-validation.ts
+++ src/validation/validators/builder/builder-validation.ts
@@ -1,5 +1,6 @@
 import { FieldValidation } from '@/validation/protocols/field-validation';
 import { RequiredFieldValidation } from '../required-field/required-field-validation';
+import { EmailValidation } from '../email/email-validation';
 
 export class ValidationBuilder {
 	private constructor(private readonly fielName: string, private readonly validations: FieldValidation[]) {}
@@ -10,6 +11,11 @@ export class ValidationBuilder {
 
 	required(): ValidationBuilder {
 		this.validations.push(new RequiredFieldValidation(this.fielName));
+		return this;
+	}
+
+	email(): ValidationBuilder {
+		this.validations.push(new EmailValidation(this.fielName));
 		return this;
 	}
 
