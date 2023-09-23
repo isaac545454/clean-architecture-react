b1941803bc38d4100d40b45c0941aa7f79c3f4ec
isaac545454 <isaac.newton1221@gmail.com>
2023-09-22 21:25:10 -0300

    test: ensure MinLenghValidation returns falsy  if values is valid

--- src/validation/validators/min-lenght/min-lenght-valitaion.spec.ts
+++ src/validation/validators/min-lenght/min-lenght-valitaion.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFielError } from '@/validation/errors';
 import { MinLenthValidation } from './min-lenght-valitaion';
 
+const makeSut = () => ({ sut: new MinLenthValidation('field', 5) });
+
 describe('minLengthValidation', () => {
 	test('should return Error if value is invalid', () => {
-		const sut = new MinLenthValidation('field', 5);
+		const { sut } = makeSut();
 		const error = sut.validate('123');
 		expect(error).toEqual(new InvalidFielError());
+	});
+	test('should return falsy if value is invalid', () => {
+		const { sut } = makeSut();
+		const error = sut.validate('12345');
+		expect(error).toBeFalsy();
 	});
 });
