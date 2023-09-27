21e09aa9d4ab07625737ac9e5845f95931da2d4d
isaac545454 <isaac.newton1221@gmail.com>
2023-09-27 19:12:00 -0300

    feat: add addAccount usecase

---
+++ src/Domain/usecases/add-account.ts
@@ -0,0 +1,12 @@
+import { AccountModel } from '@/Domain/models/accounts-model';
+
+export type AddAccountParams = {
+	email: string;
+	name: string;
+	password: string;
+	passwordConfirmation: string;
+};
+
+export interface AddAccount {
+	add(params: AddAccountParams): Promise<AccountModel>;
+}
