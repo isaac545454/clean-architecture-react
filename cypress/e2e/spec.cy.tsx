f87aa04586f245ef1f708d7699a33d06bd4af5ce
isaac545454 <isaac.newton1221@gmail.com>
2023-10-04 19:14:07 -0300

    test: ensure login present error state if form is isvalid

--- cypress/e2e/spec.cy.tsx
+++ cypress/e2e/spec.cy.tsx
@@ -1,15 +1,27 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
+import { faker } from '@faker-js/faker'
+
+describe('<Login />', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:5173/login')
 	})
-	it('passes', () => {
+	it('should load with correct initial state', () => {
 		cy.get('[data-testid="email-status"]')
 			.should('have.attr', 'title', 'campo obrigatorio')
 			.should('contain.text', '🔴')
 		cy.get('[data-testid="password-status"]')
 			.should('have.attr', 'title', 'campo obrigatorio')
+			.should('contain.text', '🔴')
+		cy.get('[data-testid="submit"]').should('have.attr', 'disabled')
+		cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
+	})
+	it('should present error state if form is invalid ', () => {
+		cy.get('[data-testid="email"]').type(faker.animal.cat())
+		cy.get('[data-testid="email-status"]').should('have.attr', 'title', 'valor invalido').should('contain.text', '🔴')
+		cy.get('[data-testid="password"]').type('123')
+		cy.get('[data-testid="password-status"]')
+			.should('have.attr', 'title', 'valor invalido')
 			.should('contain.text', '🔴')
 		cy.get('[data-testid="submit"]').should('have.attr', 'disabled')
 		cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
