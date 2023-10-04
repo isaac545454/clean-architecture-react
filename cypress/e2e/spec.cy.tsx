4e856e8bb3fbb35327166f99c73a651b25ddf332
isaac545454 <isaac.newton1221@gmail.com>
2023-10-04 19:48:12 -0300

    test: ensure login present error message if invalid credentils are provided

--- cypress/e2e/spec.cy.tsx
+++ cypress/e2e/spec.cy.tsx
@@ -1,10 +1,11 @@
 /// <reference types="cypress" />
 
 import { faker } from '@faker-js/faker'
+const baseUrl: string = 'http://localhost:5173/login'
 
 describe('<Login />', () => {
 	beforeEach(() => {
-		cy.visit('http://localhost:5173/login')
+		cy.visit(baseUrl)
 	})
 	it('should load with correct initial state', () => {
 		cy.get('[data-testid="email-status"]')
@@ -33,5 +34,21 @@ describe('<Login />', () => {
 		cy.get('[data-testid="password-status"]').should('have.attr', 'title', 'tudo certo').should('contain.text', '🟢')
 		cy.get('[data-testid="submit"]').should('not.have.attr', 'disabled')
 		cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
+	})
+	it('should present error if invalid credentials are provided ', () => {
+		cy.get('[data-testid="email"]').type(faker.internet.email())
+		cy.get('[data-testid="password"]').type(faker.internet.password())
+		cy.get('[data-testid="submit"]').click()
+		cy.get('[data-testid="error-wrap"]')
+			.get('[data-testid="spinner"]')
+			.should('exist')
+			.get('[data-testid="main-error"]')
+			.should('not.exist')
+			.get('[data-testid="spinner"]')
+			.should('not.exist')
+			.get('[data-testid="main-error"]')
+			.should('exist')
+
+		cy.url().should('eq', baseUrl)
 	})
 })
